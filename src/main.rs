ca230f751d2e44657a92c50a182acadf2342340a
S3cBar0n <secberg@gmail.com>
2021-05-15 14:10:35 -0600

    Added our new file as a mod and added the function to be used.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // Using "use" allows us to call the function as if it resides in this file.
+mod rust_data_types;
 mod rust_operators;
 use rust_operators::operators;
 
-mod rust_data_types;
-
 mod memory_management;
+mod control_flow;
 
 // THIS FILE HAS TO BE NAMED MAIN AS THE RUST COMPILER NEEDS ONE FILE TO BE MAIN
 // SO THIS FILES PURPOSE TO WILL BE TO EXECUTE OUR OTHER FILES.
@@ -16,15 +16,13 @@ fn main() {
     // Function for my test file to learning more about data types
     // rust_data_types::data_types();
 
+    println!("###### OPERATORS IN RUST ######");
+    // Function for test file for learning more about Rust Operators
+    // operators();
 
     println!("###### SCOPE AND SHADOWING IN RUST ######");
     // Function for learning about Variable Scopes and Shadowing
     // rust_data_types::scope_and_shadowing();
-
-
-    println!("###### OPERATORS IN RUST ######");
-    // Function for test file for learning more about Rust Operators
-    // operators();
 
 
     println!("###### CONSTANTS IN RUST ######");
@@ -33,5 +31,12 @@ fn main() {
 
     println!("###### MEMORY MANAGEMENT IN RUST ######");
     // memory_management::memory_management()
-    memory_management::stack_and_heap()
+    // memory_management::stack_and_heap()
+
+
+    println!("###### CONTROL FLOW IN RUST ######");
+    // control_flow::if_statement()
+    // control_flow::while_and_loop()
+    // control_flow::for_loop()
+    control_flow::match_statement()
 }
